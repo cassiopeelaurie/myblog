aa4e69148d5ee0d8b5ceafdd5fd61c64a93810f3
cassiopeelaurie <cassiopee.laurie@gmail.com>
2025-02-12 14:40:55 +0100

    quete 11 contraintes de validations

--- src/main/java/org/wildcodeschool/myblog/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-
-package org.wildcodeschool.myblog.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/wildcodeschool/myblog/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package org.wildcodeschool.myblog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.wildcodeschool.myblog.model.Image;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
